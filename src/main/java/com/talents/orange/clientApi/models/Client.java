ef15cccf0fbee420a06396f4ac2714bd9dc6b93c
Lincoln Gadea <lincoln@glinc.com.br>
2020-12-27 17:20:09 -0300

    chore: garante que a coluna 'email' seja única

--- src/main/java/com/talents/orange/clientApi/models/Client.java
+++ src/main/java/com/talents/orange/clientApi/models/Client.java
@@ -2,11 +2,13 @@ package com.talents.orange.clientApi.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name="TB_CLIENT")
@@ -20,6 +22,7 @@ public class Client implements Serializable{
 	
 	private String nome;
 	
+	@Column(name="email", unique=true)
 	private String email;
 	
 	private String cpf;
@@ -38,6 +41,7 @@ public class Client implements Serializable{
 		return nome;
 	}
 
+	@Column(unique = true)
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
